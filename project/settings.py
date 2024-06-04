ca0eab794496d0a2854e1a39feb7b73d2d73fbde
abdelrahman <abdelrahmanreda00001@gmail.com>
2024-06-04 23:28:16 +0300

    DONE

--- project/settings.py
+++ project/settings.py
@@ -130,8 +130,8 @@ STATIC_URL = 'static/'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "abdelrahmanreda0011@gmail.com"
-EMAIL_HOST_PASSWORD = "watndxwjbbepwjdo"
+EMAIL_HOST_USER = "abd@gmail.com"
+EMAIL_HOST_PASSWORD = "watnd"
 EMAIL_USE_TLS = True
 EMAIL_PORT = "587"
 
